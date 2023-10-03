36097c4101fa13dba17fcc573d17c5a83171407c
GitHubkd1294189 <kd1294189@st.kobedenshi.ac.jp>
2023-10-03 12:45:44 +0900

    ex2 completion

--- ex2/work.py
+++ ex2/work.py
@@ -2,7 +2,35 @@ from math import ceil
 
 def calc_account(m):
     # 実装は入れていません、自分で入れてください
-    pass
+    
+    #   m <= 1700　and m > 0 のとき、これがそのまま戻り値になる
+    money = 610
+    
+    #   m > 1700以上か、適切な数値か
+    if m > 1700 and m >= 1:
+        
+        #   追加料金のための計算
+        money = (m - 1700) // 315 +1 
+        
+        
+        #   これがないと(m - 1700) % 315 == 0　のときは料金が高くなる対策
+        if (m - 1700) % 315 == 0 and money > 1  :
+            money = money - 1 
+            
+        
+        #金額もとめる
+        money = money * 80 + 610
+        
+        
+    elif m < 1:
+        
+        money = None
+        
+        
+    return money
+        
+    
+    #pass
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
